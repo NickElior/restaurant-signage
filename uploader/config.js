286fc0e1cf9bbde18fba186ccb095a21d7f80923
NickElior <nicholas.safieh@elior.co.uk>
2025-04-10 15:35:36 +0100

    Update config.js

--- uploader/config.js
+++ uploader/config.js
@@ -1,5 +1,5 @@
 // Uploader config
-const GITHUB_USERNAME = "your-github-username";
-const REPO_NAME = "restaurant-signage";
-const BRANCH = "main";
-const TOKEN = "your-github-token";  // Paste your GitHub Personal Access Token here
+const GITHUB_USERNAME = NickElior;
+const REPO_NAME = restaurant-signage;
+const BRANCH = main;
+const TOKEN = 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;  // Paste your GitHub Personal Access Token here
