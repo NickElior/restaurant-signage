7f3ca844bf57423615443687fe4bda33e3a66906
NickElior <nicholas.safieh@elior.co.uk>
2025-04-10 13:43:24 +0100

    Add files via upload

---
+++ uploader/config.js
@@ -0,0 +1,5 @@
+// Uploader config
+const GITHUB_USERNAME = "your-github-username";
+const REPO_NAME = "restaurant-signage";
+const BRANCH = "main";
+const TOKEN = "your-github-token";  // Paste your GitHub Personal Access Token here
