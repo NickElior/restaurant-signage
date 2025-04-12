6c977371b130bea4f6116cd8f2bf744865b28da1
Pasha Naumov <pavel.naumov@zoa.io>
2025-04-12 15:43:42 +0100

    refactor: display dynamic urls

--- uploader/config.js
+++
@@ -1,5 +0,0 @@
-// Uploader config
-const GITHUB_USERNAME = NickElior;
-const REPO_NAME = restaurant-signage;
-const BRANCH = main;
-const TOKEN = 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;  // Paste your GitHub Personal Access Token here
